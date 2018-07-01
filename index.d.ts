3277b5db20c4e74c1eb8914cfa12f7957a414d60
Charles Liu <dodoinblue@gmail.com>
2018-07-01 11:49:22 -0700

    Add type definations

---
+++ index.d.ts
@@ -0,0 +1,6 @@
+export declare class Deferred {
+  constructor();
+}
+
+export declare function timeoutPromise(promise: Promise<any>, milliseconds: number): Promise<any>;
+export declare function delay(milliseconds: number): Promise<any>;
